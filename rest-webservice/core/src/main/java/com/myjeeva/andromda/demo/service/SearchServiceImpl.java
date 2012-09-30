618fb30acf55ca10229bba2a1414122d0b6eafd4
Jeevanandam Madanagopal <jeeva@myjeeva.com>
2012-09-30 21:37:36 +0530

    AndroMDA generated project added for REST Web services project

---
+++ rest-webservice/core/src/main/java/com/myjeeva/andromda/demo/service/SearchServiceImpl.java
@@ -0,0 +1,43 @@
+// license-header java merge-point
+/**
+ * This is only generated once! It will never be overwritten.
+ * You can (and have to!) safely modify it by hand.
+ * TEMPLATE:    SpringServiceImpl.vsl in andromda-spring cartridge
+ * MODEL CLASS: AndroMDAModel::com.myjeeva.andromda.demo::service::SearchService
+ * STEREOTYPE:  WebService
+ * STEREOTYPE:  Service
+ */
+package com.myjeeva.andromda.demo.service;
+
+import com.myjeeva.andromda.demo.vo.Document;
+import com.myjeeva.andromda.demo.vo.SearchResult;
+import java.util.List;
+
+/**
+ * @see com.myjeeva.andromda.demo.service.SearchService
+ */
+public class SearchServiceImpl
+    extends SearchServiceBase
+{
+
+    /**
+     * @see com.myjeeva.andromda.demo.service.SearchService#performSearch(String)
+     */
+    protected  SearchResult handlePerformSearch(String queryTerm)
+        throws Exception
+    {
+        // TODO implement protected  SearchResult handlePerformSearch(String queryTerm)
+        throw new UnsupportedOperationException("com.myjeeva.andromda.demo.service.SearchService.handlePerformSearch(String queryTerm) Not implemented!");
+    }
+
+    /**
+     * @see com.myjeeva.andromda.demo.service.SearchService#indexDocument(List<Document>)
+     */
+    protected  Boolean handleIndexDocument(List<Document> documents)
+        throws Exception
+    {
+        // TODO implement protected  Boolean handleIndexDocument(List<Document> documents)
+        throw new UnsupportedOperationException("com.myjeeva.andromda.demo.service.SearchService.handleIndexDocument(List<Document> documents) Not implemented!");
+    }
+
+}
